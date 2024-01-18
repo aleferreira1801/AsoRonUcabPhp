d17f2b1ec84a4da87bd8ae2e5006d9f9cef05cd4
aleferreira1801 <48299450+aleferreira1801@users.noreply.github.com>
2024-01-18 02:50:36 -0400

    cambios recientes

--- FormDetalleVenta.php
+++ FormDetalleVenta.php
@@ -1,3 +1,23 @@
+<?php
+include_once "connection.php";
+
+$query = 'SELECT id_presentacion_ron,nombre_presentacion_ron FROM presentacion_ron;';
+
+$result = $base_de_datos->query($query);
+
+// 5. Fetch the results
+// Retrieve the results of the query using functions like `pg_fetch_assoc()` or `PDOStatement::fetch()`
+
+$lugares = [];
+while ($lugar = $result->fetch()) {
+  $lugares[] = $lugar;
+}
+
+// 6. Generate HTML output
+// Within the `ListarLugar.php` file, create an HTML structure to display the list of places
+// Use a loop to iterate through the fetched results and display each place's information within appropriate HTML elements (e.g., `<ul>`, `<li>`).
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,50 +31,22 @@
     <h1 class="text-center">Nuevo Detalle de Venta</h1>
     <form action="InsertarDetalleVenta.php" method="POST">
         <div class="form-group">
-            <label for="precio_unitario_detalle_venta">Precio Unitario:</label>
-            <input required type="number" name="precio_unitario_detalle_venta" id="precio_unitario_detalle_venta" class="form-control" step="0.01">
-        </div>
         <div class="form-group">
             <label for="cant_prod_total_detalle_venta">Cantidad Total:</label>
             <input required type="number" name="cant_prod_total_detalle_venta" id="cant_prod_total_detalle_venta" class="form-control">
         </div>
-        <div class="form-group">
-            <label for="fk_id_venta">Venta:</label>
-            <input required type="integer" name="fk_id_venta" id="fk_id_venta" class="form-control">
-        </div>
-        <div class="form-group">
-            <label for="fk_id_venta">Catalogo Inventario Ron:</label>
-            <input required type="integer" name="fk_id_catalogo_invetario_ron" id="fk_id_catalogo_invetario_ron" class="form-control">
-        </div>
 
-        <div class="form-group">
-            <label for="fk_id_venta">Punto De Venta:</label>
-            <input required type="integer" name="fk_id_punto_de_venta" id="fk_id_punto_de_venta" class="form-control">
-        </div>
-        <div class="form-group">
-            <label for="fk_id_venta">Entrada:</label>
-            <input required type="integer" name="fk_id_entrada" id="fk_id_entrada" class="form-control">
-        </div>
+<div class="form-group">
+            <label for="nombre_presentacion_ron">Presentacion  Ron:</label>
 
-        <div class="form-group">
-            <label for="fk_id_venta">Evento:</label>
-            <input required type="integer" name="fk_id_evento" id="fk_id_evento" class="form-control">
+            <select class="form-control" name="nombre_presentacion_ron"  id="nombre_presentacion_ron" >
+    <?php foreach ($lugares as $lugar) { ?>
+      <option value="<?php echo $lugar['id_presentacion_ron']; ?>">
+      <?php echo $lugar['nombre_presentacion_ron']; ?></option>
+    <?php } ?>
+  </select>
         </div>
-
         
-        <div class="form-group">
-            <label for="fk_id_venta">Detalle Compra:</label>
-            <input required type="integer" name="fk_id_detalle_compra" id="fk_id_detalle_compra" class="form-control">
-        </div>
-
-        <div class="form-group">
-            <label for="fk_id_venta">Compra:</label>
-            <input required type="integer" name="fk_id_compra" id="fk_id_compra" class="form-control">
-        </div>
-        <div class="form-group">
-            <label for="fk_id_venta">Presentacion  Ron:</label>
-            <input required type="integer" name="fk_id_presentacion_ron" id="fk_id_presentacion_ron" class="form-control">
-        </div>
         <button type="submit" class="btn btn-danger">Guardar Detalle de Venta</button>
         <a href="./ListDetalleVenta.php" class="btn btn-info">Ver todos los detalles de venta</a>
     </form>
--- InsertarDetalleVenta.php
+++ InsertarDetalleVenta.php
@@ -1,28 +1,59 @@
-
 <?php
 #Salir si alguno de los datos no está presente
 
- 
-
 #Si todo va bien, se ejecuta esta parte del código...
 
-
 include_once("connection.php");
+$nombre_presentacion_ron = $_POST["nombre_presentacion_ron"];
 
-$precio_unitario_detalle_venta = $_POST["precio_unitario_detalle_venta"];
+# Obtener el precio unitario
+$sql = "SELECT precio_presentacion_ron
+FROM detalle_venta, presentacion_ron
+WHERE id_detalle_venta = fk_id_presentacion_ron and
+    nombre_presentacion_ron = '$nombre_presentacion_ron'";
+$stmt = $base_de_datos->prepare($sql);
+$stmt->execute();
+$precio_unitario_detalle_venta = $stmt->fetchColumn();
+
 $cant_prod_total_detalle_venta = $_POST["cant_prod_total_detalle_venta"];
-$fk_id_venta = $_POST["fk_id_venta"];
-$fk_id_catalogo_inventario_ron = $_POST["fk_id_catalogo_inventario_ron"];
-$fk_id_punto_de_venta = $_POST["fk_id_punto_de_venta"];
-$fk_id_entrada = $_POST["fk_id_entrada"];
-$fk_id_evento = $_POST["fk_id_evento"];
-$fk_id_detalle_compra = $_POST["fk_id_detalle_compra"];
-$fk_id_compra = $_POST["fk_id_compra"];
-$fk_id_presentacion_ron = $_POST["fk_id_presentacion_ron"];
+# Obtener el precio unitario
+$sql = "SELECT id_venta
+FROM venta
+ORDER BY id_venta DESC
+LIMIT 1";
+$stmt = $base_de_datos->prepare($sql);
+$stmt->execute();
+$fk_id_venta = $stmt->fetchColumn();
 
+$sql = "SELECT id_presentacion_ron
+FROM presentacion_ron
+WHERE nombre_presentacion_ron = '$nombre_presentacion_ron'";
+$stmt = $base_de_datos->prepare($sql);
+$stmt->execute();
+$fk_id_presentacion_ron = $stmt->fetchColumn();
 
+$sql = "SELECT id_catalogo_inventario_ron
+FROM catalogo_inventario_ron
+WHERE id_catalogo_inventario_ron = '$fk_id_presentacion_ron'";
+$stmt = $base_de_datos->prepare($sql);
+$stmt->execute();
+$fk_id_catalogo_inventario_ron = $stmt->fetchColumn();
 
+$fk_id_punto_de_venta = 1;
 
+$sql = "SELECT id_detalle_compra
+FROM detalle_compra
+WHERE id_detalle_compra = '$fk_id_presentacion_ron'";
+$stmt = $base_de_datos->prepare($sql);
+$stmt->execute();
+$fk_id_detalle_compra = $stmt->fetchColumn();
+
+$sql = "SELECT fk_id_compra
+FROM detalle_compra 
+WHERE  $fk_id_detalle_compra = id_detalle_compra";
+$stmt = $base_de_datos->prepare($sql);
+$stmt->execute();
+$fk_id_compra = $stmt->fetchColumn();
 
 /*
 Al incluir el archivo "base_de_datos.php", todas sus variables están
@@ -34,35 +65,28 @@ $sentencia = $base_de_datos->prepare(
         precio_unitario_detalle_venta,
         cant_prod_total_detalle_venta,
         fk_id_venta,
+        fk_id_presentacion_ron,
         fk_id_catalogo_inventario_ron,
         fk_id_punto_de_venta,
-        fk_id_entrada,
-        fk_id_evento,
         fk_id_detalle_compra,
-        fk_id_compra,
-        fk_id_presentacion_ron
-    ) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);");
-  
+        fk_id_compra
+    ) values (?, ?, ?, ?, ?, ?, ?, ?);
+    "
+);
 
-  
-  #execute regresa un booleano. True en caso de que todo vaya bien, falso en caso contrario.
-  #Con eso podemos evaluar
-  
-  
- $resultadoCompra = $sentencia->execute(
+$resultadoCompra = $sentencia->execute(
     [
         $precio_unitario_detalle_venta,
         $cant_prod_total_detalle_venta,
         $fk_id_venta,
+        $fk_id_presentacion_ron,
         $fk_id_catalogo_inventario_ron,
         $fk_id_punto_de_venta,
-        $fk_id_entrada,
-        $fk_id_evento,
         $fk_id_detalle_compra,
         $fk_id_compra,
-        $fk_id_presentacion_ron,
     ]
 );
+
 
 #execute regresa un booleano. True en caso de que todo vaya bien, falso en caso contrario.
 #Con eso podemos evaluar
